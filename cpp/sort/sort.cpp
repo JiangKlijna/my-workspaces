08347ba1b2eed6c652ca4d7ce365ef4806bfa91a
jiangKlijna <leil729564090@outlook.com>
2017-12-22 20:13:43 +0800

    :fire:add sort.cpp bubblesort

--- cpp/sort/sort.cpp
+++ cpp/sort/sort.cpp
@@ -42,4 +42,13 @@ void IntArray::print() {
 
 // 冒泡排序
 void BubbleSort::sort(IntArray arr) {
+	for (int i = arr.size() - 1; i >= 0; i--) {
+		for (int j = 0; j < i; j++) {
+			if (arr[j] > arr[j + 1]) {
+				int temp = arr[j + 1];
+				arr[j + 1] = arr[j];
+				arr[j] = temp;
+			}
+		}
+	}
 }
