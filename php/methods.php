bcef20dd54f8629138d64c63d7a840ed640cc6fc
jiangKlijna <leil729564090@outlook.com>
2018-02-03 17:23:44 +0800

    :fire:update

--- php/methods.php
+++ php/methods.php
@@ -1,10 +1,15 @@
 <?php
 header('Content-Type:application/json');
+if (empty($_POST)) {
+	$_DATA = array();
+	parse_str(file_get_contents('php://input'), $_DATA);
+}
 $arr = array(
 	"server"=>$_SERVER,
 	"get"=>$_GET,
 	"post"=>$_POST,
 	"file"=>$_FILES,
+	"data"=>$_DATA,
 );
 echo json_encode($arr);
 ?>
