f62ba57bf723996993d97aaa1c4cb9c2e9ee27d3
jiangKlijna <leil729564090@outlook.com>
2018-02-16 21:33:32 +0800

    :fire:compile

--- go/compile.go
+++ go/compile.go
@@ -2,136 +2,174 @@
 package main
 
 import (
-  "os"
-  "fmt"
-  "strings"
+	"os"
+	"fmt"
+	"bytes"
 	"net/http"
-  "io/ioutil"
-  "encoding/json"
+	"io/ioutil"
+	"encoding/json"
 )
 
-var info = `
-  compile [language] [filename]
+const info = `run a single code file
+compile [language] [filename]
 
-  Example:
-    compile java Test.java
-    compile swift hello.swift
+Example:
+	compile java Test.java
+	compile swift hello.swift
 
-  Supported language:
-    php
-    pascal
-    lua
-    node.js
-    go
-    swift
-    vb.net
-    perl
-    objectiveC
-    scala
-    c#
-    ruby
-    c++
-    c
-    java
-    python
+Language:
+	php
+	python
+	java
+	c
+	c++
+	ruby
+	c#
+	scala
+	objc
+	perl
+	bash
+	vb.net
+	swift
+	go
+	node
+	lua
+	pascal
+	kotlin
 `
 
-var langs_str = `{"php":"3","pascal":"18","lua":"17","node.js":"4","go":"6","swift":"16","vb.net":"9","perl":"14","objectiveC":"12","scala":"5","c#":"10","ruby":"1","c++":"7","c":"7","java":"8","python":"15"}`
-var langs map[string]string
+type Result struct {
+	Output string `json:"output"`
+	Langid int    `json:"langid"`
+	Code   string `json:"code"`
+	Errors string `json:"errors"`
+	Time   string `json:"time"`
+}
+
+func (self *Result) String() string {
+	str, err := json.Marshal(*self)
+	if err != nil {
+		return err.Error()
+	} else {
+		return string(str)
+	}
+}
 
 type Compile struct {
-  lang string
+	lang_str string
+	lang_num string
 }
 
-//
-func NewCompile() *Compile {
-  lang := test_args()
-  return &Compile{lang}
+var compiles = map[string]*Compile{
+	"php":    &Compile{"php", "3"},
+	"python": &Compile{"python3", "15"},
+	"java":   &Compile{"java", "8"},
+	"c":      &Compile{"c", "7"},
+	"c++":    &Compile{"c++", "7"},
+	"ruby":   &Compile{"ruby", "1"},
+	"c#":     &Compile{"c#", "10"},
+	"scala":  &Compile{"scala", "5"},
+	"objc":   &Compile{"objc", "12"},
+	"perl":   &Compile{"perl", "14"},
+	"bash":   &Compile{"bash", "11"},
+	"vb.net": &Compile{"vb.net", "9"},
+	"swift":  &Compile{"swift", "16"},
+	"go":     &Compile{"go", "6"},
+	"node":   &Compile{"node", "4"},
+	"lua":    &Compile{"lua", "17"},
+	"pascal": &Compile{"pascal", "18"},
+	"kotlin": &Compile{"kotlin", "19"},
 }
 
-func (self *Compile) Params() (string, error) {
-  filename := os.Args[2]
-  f, err := os.Open(filename)
-  if err != nil {
-    fmt.Println("File not found : " + filename)
-    return filename, err
-  }
-  data, err := ioutil.ReadAll(f)
-  if err != nil {
-    fmt.Println("File read failed : " + filename)
-    return filename, err
-  }
-  return "language=" + self.lang + "&code=" + string(data), nil
+func (self *Compile) Params() ([]byte, error) {
+	filename := os.Args[2]
+	f, err := os.Open(filename)
+	if err != nil {
+		fmt.Println("File not found :", filename)
+		return []byte{}, err
+	}
+	data, err := ioutil.ReadAll(f)
+	if err != nil {
+		fmt.Println("File read failed :", filename)
+		return data, err
+	}
+	buf := bytes.Buffer{}
+	buf.WriteString("language=")
+	buf.WriteString(self.lang_num)
+	buf.WriteString("&code=")
+	buf.Write(data)
+	return buf.Bytes(), nil
 }
 
-func (self *Compile) Respose(params string) (*http.Response, error) {
-  req, _ := http.NewRequest("POST", "http://tool.runoob.com/compile.php", strings.NewReader(params))
-  req.Header.Add("User-Agent", "Mozilla/5.0 (Windows NT 6.1; WOW64; Trident/7.0; rv:11.0) like Gecko")
-  req.Header.Add("Content-Type", "application/x-www-form-urlencoded; charset=UTF-8")
-  req.Header.Add("Host", "tool.runoob.com")
-  req.Header.Add("Origin", "http://c.runoob.com")
-  req.Header.Add("Referer", "http://c.runoob.com/compile/" + self.lang)
+func (self *Compile) Request(params []byte) (*http.Response, error) {
+	req, _ := http.NewRequest("POST", "https://m.runoob.com/api/compile.php", bytes.NewReader(params))
+	req.Header.Add("User-Agent", "Mozilla/5.0 (Windows NT 6.1; WOW64; Trident/7.0; rv:11.0) like Gecko")
+	req.Header.Add("Content-Type", "application/x-www-form-urlencoded; charset=UTF-8")
+	req.Header.Add("Host", "m.runoob.com")
+	req.Header.Add("Origin", "https://c.runoob.com")
+	req.Header.Add("Referer", "https://c.runoob.com/compile/"+self.lang_num)
 
-  resp, err := (&http.Client{}).Do(req)
-  if err != nil {
-    fmt.Println("Network connection is failed")
-  }
-  return resp, err
+	resp, err := (&http.Client{}).Do(req)
+	if err != nil {
+		fmt.Println("request error", err.Error())
+	}
+	return resp, err
 }
 
-func (self *Compile) Analysi(resp *http.Response) (*map[string]string, error) {
-  defer resp.Body.Close()
-  body, err := ioutil.ReadAll(resp.Body)
-  if err != nil {
-    fmt.Println("Server error 500")
-    return nil, err
-  }
-  // fmt.Println(string(body))
-
-  var res map[string]string
-  json.Unmarshal(body, &res)
-
-  return &res, nil
+func (self *Compile) Analyse(resp *http.Response) (*Result, error) {
+	defer resp.Body.Close()
+	body, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		fmt.Println("server error", err.Error())
+		return nil, err
+	}
+	var res Result
+	err = json.Unmarshal(body, &res)
+	if err != nil {
+		fmt.Println("result error", err.Error())
+		return nil, err
+	}
+	return &res, nil
 }
 
 func (self *Compile) Run() int {
-  params, err := self.Params()
-  if err != nil {
-    return 1
-  }
-  reps, err := self.Respose(params)
-  if err != nil {
-    return 2
-  }
-  res, err := self.Analysi(reps)
-  if err != nil {
-    return 3
-  }
-  if (*res)["output"] == "" {
-    fmt.Print((*res)["errors"])
-  } else {
-    fmt.Print((*res)["output"])
-  }
-  return 0
+	params, err := self.Params()
+	if err != nil {
+		return 1
+	}
+	fmt.Println(string(params))
+	reps, err := self.Request(params)
+	if err != nil {
+		return 2
+	}
+	res, err := self.Analyse(reps)
+	if err != nil {
+		return 3
+	}
+	fmt.Println(res)
+	//if (*res)["output"] == "" {
+	//	fmt.Print((*res)["errors"])
+	//} else {
+	//	fmt.Print((*res)["output"])
+	//}
+	return 0
 }
 
-func init() {
-  json.Unmarshal([]byte(langs_str), &langs)
+func getCompile() *Compile {
+	n := len(os.Args)
+	if n != 3 {
+		fmt.Print(info)
+		os.Exit(-1)
+	}
+	compile, isNil := compiles[os.Args[1]]
+	if !isNil {
+		fmt.Print(info)
+		os.Exit(-2)
+	}
+	return compile
 }
 
 func main() {
-  os.Exit(NewCompile().Run())
-}
-
-func test_args() string {
-  if len(os.Args) < 3 {
-    fmt.Print(info)
-    os.Exit(-1)
-  }
-  lang, isNil := langs[os.Args[1]]
-  if !isNil {
-    fmt.Print(info)
-    os.Exit(-2)
-  }
-  return lang
+	compile := getCompile()
+	os.Exit(compile.Run())
 }
