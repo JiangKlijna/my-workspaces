4ffdd8988bf6a9b701c0ef7cd6f9b89f975074f2
jiangKlijna <leil729564090@outlook.com>
2018-06-21 20:23:29 +0800

    :fire:add logging

--- go/HttpServer.go
+++ go/HttpServer.go
@@ -1,8 +1,43 @@
+//go build -ldflags "-s -w" HttpServer.go
 package main
 import (
-  "net/http"
+	"os"
+	"fmt"
+	"time"
+	"net/http"
+	"strconv"
 )
+
+func isNumber(s string) bool {
+	_, err := strconv.Atoi(s)
+	return err == nil
+}
+
+func getPort() string {
+	if len(os.Args) > 1 && isNumber(os.Args[1]) {
+		return os.Args[1]
+	}
+	return "5050"
+}
+
+func loggingHandler(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		start := time.Now()
+		next.ServeHTTP(w, r)
+		str := fmt.Sprintf(
+			"%s Comleted %s %s in %v from %s\n",
+			start.Format("2006-01-02 15:04:05"),
+			r.Method,
+			r.URL.Path,
+			time.Since(start),
+			r.RemoteAddr)
+		fmt.Print(str)
+	})
+}
+
 func main() {
-  http.Handle("/", http.FileServer(http.Dir("./")))
-  http.ListenAndServe(":5050", nil)
+	port := getPort()
+	fmt.Println("> Listening at http://127.0.0.1:" + port)
+	http.Handle("/", loggingHandler(http.FileServer(http.Dir("./"))))
+	http.ListenAndServe(":" + port, nil)
 }
