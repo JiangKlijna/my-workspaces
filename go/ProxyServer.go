67a2e00bc014c193de2b92c61f45bf98ea10d4ab
jiangKlijna <leil729564090@outlook.com>
2018-06-22 21:44:04 +0800

    :fire:add ProxyServer.go

---
+++ go/ProxyServer.go
@@ -0,0 +1,119 @@
+package main
+
+import (
+	"fmt"
+	"time"
+	"strconv"
+	"net/http"
+	"net/http/httputil"
+	"net/url"
+	"strings"
+	"os"
+	"math/rand"
+)
+
+func isNumber(s string) bool {
+	_, err := strconv.Atoi(s)
+	return err == nil
+}
+
+func getPorts() (string, []string) {
+	if len(os.Args) > 2 {
+		ports := make([]string, 0)
+		arr := strings.Split(os.Args[2], ",")
+		for _, p := range arr {
+			if isNumber(p) {
+				ports = append(ports, p)
+			}
+		}
+		if len(arr) == 0 {
+			return os.Args[1], []string{"8080", "8081", "8082"}
+		} else {
+			return os.Args[1], ports
+		}
+	}
+	return "5050", []string{"8080", "8081", "8082"}
+}
+
+func getReverseProxies(ports []string) []http.Handler {
+	hs := make([]http.Handler, len(ports))
+	for i, port := range ports {
+		targetUrl, _ := url.Parse("http://127.0.0.1:" + port)
+		hs[i] = httputil.NewSingleHostReverseProxy(targetUrl)
+	}
+	return hs
+}
+
+// iphash
+func clusterHandlerIphash(hs []http.Handler) http.Handler {
+	n := uint32(len(hs))
+	const primeRK = 16777619
+	// 针对Remote Address所写的hash算法
+	hashStr := func(str string) uint32 {
+		hash := uint32(0)
+		for _, v := range str {
+			if v == ':' {
+				return hash
+			}
+			hash = hash*primeRK + uint32(v)
+		}
+		return hash
+	}
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		hs[hashStr(r.RemoteAddr)%n].ServeHTTP(w, r)
+	})
+}
+
+// 随机
+func clusterHandlerRandom(hs []http.Handler) http.Handler {
+	n := len(hs)
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		hs[rand.Intn(n)].ServeHTTP(w, r)
+	})
+}
+
+// 轮询
+func clusterHandlerRound(hs []http.Handler) http.Handler {
+	n := len(hs)
+	i := 0
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		if i == n {
+			i = 0
+		}
+		hs[i].ServeHTTP(w, r)
+		i++
+	})
+}
+
+func clusterHandler(hs []http.Handler) http.Handler {
+	n := len(hs)
+	if n == 1 {
+		return hs[0]
+	}
+	fs := [3]func(hs []http.Handler) http.Handler{
+		clusterHandlerRound, clusterHandlerRandom, clusterHandlerIphash,
+	}
+	return fs[rand.Intn(3)%3](hs)
+}
+
+func loggingHandler(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		start := time.Now()
+		next.ServeHTTP(w, r)
+		str := fmt.Sprintf(
+			"%s Comleted %s %s in %v from %s\n",
+			start.Format("2006-01-02 15:04:05"),
+			r.Method,
+			r.URL.Path,
+			time.Since(start),
+			r.RemoteAddr)
+		fmt.Print(str)
+	})
+}
+
+func main() {
+	listeningPort, proxyPort := getPorts()
+	fmt.Println("> Listening at http://127.0.0.1:" + listeningPort)
+	http.Handle("/", loggingHandler(clusterHandler(getReverseProxies(proxyPort))))
+	http.ListenAndServe(":"+listeningPort, nil)
+}
