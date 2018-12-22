5112e6d5d61af58f5791403f3b4717cefebafd80
jiangKlijna <leil729564090@outlook.com>
2018-12-22 21:00:05 +0800

    :fire:adjust

--- java/jni/run_win_gcc.bat
+++ java/jni/run_win_gcc.bat
@@ -1,7 +1,9 @@
 @echo off
-set jdk_home="D:\Program Files\Java\jdk1.8.0_151"
 
-javac JniTest.java
-javah JniTest
-gcc -I %jdk_home%\include -I %jdk_home%\include\win32 -shared -s -o Jni.dll JniTest.c
-java JniTest
+echo %JAVA_HOME%
+
+%JAVA_HOME%\bin\javac JniTest.java
+%JAVA_HOME%\bin\javac -h . JniTest.java
+%JAVA_HOME%\bin\javah JniTest
+gcc -I %JAVA_HOME%\include -I %JAVA_HOME%\include\win32 -shared -s -o Jni.dll JniTest.c
+%JAVA_HOME%\bin\java JniTest
