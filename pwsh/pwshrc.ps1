6a74b90f96d9d982608f4c0c86ad229cd7c53bad
jiangKlijna <leil729564090@outlook.com>
2020-11-16 11:53:24 +0800

    :cat:add voice function

--- pwsh/pwshrc.ps1
+++ pwsh/pwshrc.ps1
@@ -1,14 +1,19 @@
 
 
+# (Invoke-WebRequest -Uri "https://raw.githubusercontent.com/JiangKlijna/my-workspaces/master/pwsh/pwshrc.ps1").Content > $profile
 
 function listfile([int]$col = 5) {
     Get-ChildItem | Format-Wide -Column $col -Property Name
 }
 
+function voice {
+    param([string]$text)
+    (New-Object -ComObject Sapi.SpVoice).Speak($text) | out-null
+}
+
 function getjson($f) {
     (Get-Content $f) | ConvertFrom-Json
 }
-
 
 function tojson($obj) {
     ConvertTo-Json -InputObject $obj
