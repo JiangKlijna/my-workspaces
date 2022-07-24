983a728181482cfef8d9c08bb7726d80b91dee3e
jiangKlijna <leil729564090@outlook.com>
2022-07-24 13:18:41 +0800

    pwsh plus

--- pwsh/pwshrc.ps1
+++ pwsh/pwshrc.ps1
@@ -6,38 +6,43 @@ function Show-File([int]$col = 5) {
     Get-ChildItem | Format-Wide -Column $col -Property Name
 }
 
-function Start-Voice {
+function Get-Voice {
     param([string]$text)
     (New-Object -ComObject Sapi.SpVoice).Speak($text) | out-null
 }
 
-function Get-Files {
-    param([string]$Path)
-    $dirs = Get-ChildItem -Path $Path -Directory -Name
-    if ($dirs) {
-        if ($dirs.GetType() -eq "".GetType()) {
-            Get-Files("$Path\$dirs")
-        }
-        else {
-            for ($i = 0; $i -lt $dirs.Count; $i++) {
-                $dir = $dirs[$i]
-                Get-Files("$Path\$dir")
-            }
-        }
-    }
-    $fs = Get-ChildItem -Path $Path -File -Name
-    if ($fs) {
-        if ($fs.GetType() -eq "".GetType()) {
-            Write-Output "$Path\$fs"
-        }
-        else {
-            for ($i = 0; $i -lt $fs.Count; $i++) {
-                $f = $fs[$i]
-                Write-Output "$Path\$f"
-            }
-        }
-    }
+function Get-Goto([string]$text) {
+	Start-Process -FilePath $text
 }
+
+function Get-Baidu([string]$text) {
+	goto https://www.baidu.com/s?wd=$text
+}
+
+function Get-Bing([string]$text) {
+	goto https://cn.bing.com/search?q=$text
+}
+
+function Get-Github([string]$text) {
+	goto https://github.com/search?q=$text
+}
+
+function Get-Google([string]$text) {
+	goto https://www.google.com.hk/search?q=$text
+}
+
+function Get-P([string]$link) {
+	$uri=([uri]$link)
+	#$uri=[System.Uri]::new($link)
+	if ($uri.IsAbsoluteUri) {
+		$urihost = $uri.Host
+	} else {
+		$urihost = $link
+	}
+	echo $urihost
+	ping $urihost
+}
+
 
 Remove-Item alias:ls
 Set-Alias ll Get-ChildItem
